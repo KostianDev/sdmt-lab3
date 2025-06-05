fa4d5e2fa2a21eb14832910ad6c1200ed0d4c479
KostianDev <verbytskyi.kostiantyn@lll.kpi.ua>
2025-06-05 12:18:12 +0300

    Implement matrix multiplication

--- spaceship/routers/api.py
+++ spaceship/routers/api.py
@@ -1,4 +1,5 @@
 from fastapi import APIRouter
+import numpy as np
 
 router = APIRouter()
 
@@ -6,3 +7,13 @@ router = APIRouter()
 @router.get('')
 def hello_world() -> dict:
     return {'msg': 'Hello, World!'}
+
+@router.get("/matrix-mult")
+def matrix_mult():
+    a = np.random.randint(0, 10, (10, 10))
+    b = np.random.randint(0, 10, (10, 10))
+    return {
+        "matrix_a": a.tolist(),
+        "matrix_b": b.tolist(),
+        "product":  (a @ b).tolist(),
+    }
